9333ea9a2c332a015e2d70614581561be7e20ea3
Tomas Bures <bures.tomas@live.com>
2025-06-04 15:55:09 +0200

    purpose edit

--- Image-Cropper/Program.cs
+++ Image-Cropper/Program.cs
@@ -14,246 +14,102 @@ using System.Linq;
 
 namespace Image_Cropper
 {
-    class Program
+  class Program
+  {
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
+      //Source images file extension allowed.
+      List<string> fileExtensionsList = new List<string> { ".png", ".jpg" };
+      string[] imageFiles;
+
+      Console.WriteLine("<<< IMAGE CROPPER TOOL >>>\n");
+      Console.Write("\n\n");
+
+      // Directory path where the .exe file is executed.
+      //string dir = Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().Location);
+      string dir = @"D:\@Git\lsrp\image-server\data\uploads\vehicles";
+
+      // Get all subdirectories
+      string[] subdirectories = Directory.GetDirectories(dir);
+
+      foreach (string subdir in subdirectories)
+      {
+        Console.WriteLine($"Subfolder: " + Path.GetFileName(subdir));
+
+        imageFiles = Directory.GetFiles(Path.Combine(dir, subdir), "*.*", SearchOption.TopDirectoryOnly)
+            .Where(s => fileExtensionsList.Contains(Path.GetExtension(s.ToLower()))).ToArray();
+
+        //Crop Images
+        CropImagesCenter(imageFiles, dir, subdir);
+      }
+
+      Console.WriteLine("\n\nAll images cropped!\n");
+      Console.Read();
+
+      void CropImagesCenter(string[] images, string currentDir, string subFolder)
+      {
+        if (images.Length > 0)
         {
-            //Minimum width of the source image.
-            int minImageWidth = 3840; //1920, 3840
+          Console.Write("\n");
 
-            //Source images file extension allowed.
-            List<string> fileExtensionsList = new List<string> { ".png", ".jpg" };
-
-            //List of cropped images.
-            List<string> croppedImagesList = new List<string>();
-
-            //Counts the number of images cropped.
-            int imagesCroppedCount = 0;
-            string[] imageFiles;
-            string cropConfirmation;
-
-            //Welcome and information
-            Console.WriteLine("<<< IMAGE CROPPER TOOL >>>\n");
-            Console.WriteLine(" - This program will crop images in half and save both sides.");
-            Console.WriteLine(" - Crop all images in the same directory where the .exe file is executed.");
-            Console.WriteLine(" - Or crop the images dropped on the .exe file.");
-            Console.WriteLine(" - Will only crop images equal or larger than " + minImageWidth + " pixels width.");
-            if (args.Length <= 0)
-                Console.WriteLine(" - Then the program asks if you want to delete the original files."); 
-
-            Console.Write("\n\n");
-
-
-            //If some file is dropped on the .exe file.
-            if (args.Length > 0)
+          foreach (string img in images)
+          {
+            //Fast way to check image sizes.
+            using (Stream stream = File.OpenRead(img))
             {
-                //List of all images dropped with the extensions in "fileExtensions".
-                imageFiles = args.Where(s => fileExtensionsList.Contains(Path.GetExtension(s.ToLower()))).ToArray();
-
-                if (imageFiles.Length > 0)
-                {
-                    //Crop Images
-                    cropConfirmation = string.Empty;
-
-                    Console.WriteLine("Images to crop:");
-                    foreach (string images in imageFiles)
-                        Console.WriteLine(" - " + Path.GetFileName(images));
-
-                    //While the user input is different than "y" or "n" keeps asking for crop confirmation.
-                    while (cropConfirmation != "y" && cropConfirmation != "n")
-                    {
-                        Console.Write("Crop images: [y/n]? ");
-                        cropConfirmation = Console.ReadLine().ToLower();
-                    }
-
-                    //If the user input is "y" then...
-                    if (cropConfirmation == "y")
-                    {
-                        //Crop Images
-                        CropImagesInHalf(imageFiles);
-
-                        //If one or more images were cropped then...
-                        if (imagesCroppedCount > 0)
-                        {
-                            //Console message with the number of images cropped.
-                            Console.WriteLine("\n" + imagesCroppedCount + " Images were cropped!\n");
-                            Console.WriteLine("Thank you!");
-                        }
-                        else
-                        {
-                            //No images where cropped. Show console messages.
-                            Console.WriteLine("No images where cropped!");
-                            Console.WriteLine("\nExiting...");
-                        }
-                    }
-                    else
-                    {
-                        //If the user input is "n" do nothing and show console messages.
-                        Console.WriteLine("\nImages no cropped!");
-                        Console.WriteLine("\nExiting...");
-                    }
-                }
-                else
-                {
-                    //No images where cropped. Show console messages.
-                    Console.WriteLine("No images to crop found!");
-                    Console.WriteLine("\nExiting...");
-                }
+              using (Image sourceImage = Image.FromStream(stream, false, false))
+              {
+                CropImage(img, currentDir, subFolder);
+              }
             }
-            else
-            {
-                //If no file was dropped.
+          }
+        }
+      }
 
-                //Directory path where the .exe file is executed.
-                string dir = Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().Location);
+      void CropImage(string image, string currentDir, string subFolder)
+      {
+        //Creates a bitmap image from source image.
+        Bitmap srcImg = Image.FromFile(image) as Bitmap;
 
-                //List of all images found with the extensions in "fileExtensions" in the directory "dir". 
-                imageFiles = Directory.GetFiles(dir, "*.*", SearchOption.TopDirectoryOnly)
-                        .Where(s => fileExtensionsList.Contains(Path.GetExtension(s.ToLower()))).ToArray();
+        Rectangle cropRect = new Rectangle(srcImg.Size.Width/4, srcImg.Size.Height/4, srcImg.Size.Width /2, srcImg.Size.Height /2);
+        Bitmap targetImg = new Bitmap(cropRect.Width, cropRect.Height);
 
-                //Stores the user input of the crop confirmation.
-                cropConfirmation = string.Empty;
+        //Crop left side of the image.
+        using (Graphics g = Graphics.FromImage(targetImg))
+        {
+          g.DrawImage(srcImg, new Rectangle(0, 0, targetImg.Width, targetImg.Height),
+                    cropRect,
+                    GraphicsUnit.Pixel);
+        }
 
-                //While the user input is different than "y" or "n" keeps asking for crop confirmation.
-                while (cropConfirmation != "y" && cropConfirmation != "n")
-                {
-                    Console.Write("Crop all images in \"" + dir + "\" [y/n]? ");
-                    cropConfirmation = Console.ReadLine().ToLower();
-                }
+        var savePath = Path.Combine(dir, "@cropped", Path.GetFileName(subFolder));
+        Directory.CreateDirectory(savePath);
+        targetImg.Save(Path.Combine(savePath, "medium_" + Path.GetFileName(image)), ImageFormat.Png);
 
-                //If the user input is "y" then...
-                if (cropConfirmation == "y")
-                {
-                    //Crop Images
-                    CropImagesInHalf(imageFiles);
+        var scaledImg = ScaleBitmap(targetImg, 0.5f);
+        scaledImg.Save(Path.Combine(savePath, "mini_" + Path.GetFileName(image)), ImageFormat.Png);
 
-                    //If one or more images were cropped then...
-                    if (imagesCroppedCount > 0)
-                    {
-                        //Console message with the number of images cropped.
-                        Console.WriteLine("\n" + imagesCroppedCount + " Images were cropped!\n");
+        Console.WriteLine(Path.GetFileName(image) + " Cropped!");
 
-                        //Stores the user input of the delete original images confirmation.
-                        string deleteConfirmation = string.Empty;
+        //Release the bitmap image used.
+        srcImg.Dispose();
+      }
 
-                        //While the user input is different than "y" or "n" keeps asking for delete original images confirmation.
-                        while (deleteConfirmation != "y" && deleteConfirmation != "n")
-                        {
-                            Console.Write("Delete original files [y/n]? ");
-                            deleteConfirmation = Console.ReadLine().ToLower();
-                        }
+      Bitmap ScaleBitmap(Bitmap original, float scale)
+      {
+        int newWidth = (int)(original.Width * scale);
+        int newHeight = (int)(original.Height * scale);
+        Bitmap resized = new Bitmap(newWidth, newHeight);
 
-                        //If the user input is "y" then...
-                        if (deleteConfirmation == "y")
-                        {
-                            //Console message.
-                            Console.WriteLine("\nDeleting original files...");
+        using (Graphics g = Graphics.FromImage(resized))
+        {
+          g.InterpolationMode = System.Drawing.Drawing2D.InterpolationMode.HighQualityBicubic;
+          g.DrawImage(original, 0, 0, newWidth, newHeight);
+        }
 
-                            //For each image cropped...
-                            foreach (string imageFile in croppedImagesList)
-                            {
-                                //Check if image file exists. Then delete the file.
-                                if (File.Exists(imageFile))
-                                {
-                                    Console.WriteLine(Path.GetFileName(imageFile) + " Deleted!");
-                                    File.Delete(imageFile);
-                                }
-                            }
+        return resized;
+      }
 
-                            //Once done show console messages.
-                            Console.WriteLine("\nDeleting Done!");
-                            Console.WriteLine("Thank you!");
-                        }
-                        else
-                        {
-                            //If the user input is "n" don't delete image files and show console messages.
-                            Console.WriteLine("\nOriginal files not deleted!");
-                            Console.WriteLine("Thank you!");
-                        }
-                    }
-                    else
-                    {
-                        //No images where cropped. Show console messages.
-                        Console.WriteLine("No images to crop found!");
-                        Console.WriteLine("\nExiting...");
-                    }
-                }
-                else
-                {
-                    //If the user input is "n" do nothing and show console messages.
-                    Console.WriteLine("\nImages no cropped!");
-                    Console.WriteLine("\nExiting...");
-                }
-            }
-            Console.Read();
-
-            void CropImagesInHalf(string[] images)
-            {
-                if (images.Length > 0)
-                {
-                    Console.Write("\n");
-
-                    foreach (string img in images)
-                    {
-						//Fast way to check image sizes.
-						using (Stream stream = File.OpenRead(img))
-						{
-							using (Image sourceImage = Image.FromStream(stream, false, false))
-							{
-								//If the width of the source image is at least or greater than the "minImageWidth" then crop images...
-								if (sourceImage.Width >= minImageWidth)
-									CropImage(img);
-							}
-						}
-					}
-				}
-            }
-
-			void CropImage(string image)
-			{
-				//Creates a bitmap image from source image.
-				Bitmap srcImg = Image.FromFile(image) as Bitmap;
-
-				//Variables for the left side.
-				Rectangle cropRectLeft = new Rectangle(0, 0, srcImg.Size.Width / 2, srcImg.Size.Height);
-				Bitmap targetImgLeft = new Bitmap(cropRectLeft.Width, cropRectLeft.Height);
-
-				//Variables for the right side.
-				Rectangle cropRectRight = new Rectangle(srcImg.Size.Width / 2, 0, srcImg.Size.Width / 2, srcImg.Size.Height);
-				Bitmap targetImgRight = new Bitmap(cropRectRight.Width, cropRectRight.Height);
-
-				//Crop left side of the image.
-				using (Graphics g = Graphics.FromImage(targetImgLeft))
-				{
-					g.DrawImage(srcImg, new Rectangle(0, 0, targetImgLeft.Width, targetImgLeft.Height),
-										cropRectLeft,
-										GraphicsUnit.Pixel);
-				}
-				//Saves the left side of the image as png.
-				targetImgLeft.Save(Path.GetFileNameWithoutExtension(image) + "_1.png", ImageFormat.Png);
-
-				//Crop right side of the image.
-				using (Graphics g = Graphics.FromImage(targetImgRight))
-				{
-					g.DrawImage(srcImg, new Rectangle(0, 0, targetImgRight.Width, targetImgRight.Height),
-										cropRectRight,
-										GraphicsUnit.Pixel);
-				}
-				//Saves the right side of the image as png.
-				targetImgRight.Save(Path.GetFileNameWithoutExtension(image) + "_2.png", ImageFormat.Png);
-
-				//Increment the images cropped.
-				imagesCroppedCount++;
-
-				//Print the name of the image cropped.
-				Console.WriteLine(imagesCroppedCount + "- Image: " + Path.GetFileName(image) + " Cropped!");
-
-				//Add cropped image to "croppedImagesList".
-				croppedImagesList.Add(image);
-
-				//Release the bitmap image used.
-				srcImg.Dispose();
-			}
-		}
     }
+  }
 }
